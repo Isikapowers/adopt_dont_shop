bca824cc9042884ee1dd8d47476fe54e4fccf120
iPowers <isika.powers@gmail.com>
2021-09-07 14:16:05 -0600

    Fix approve/reject feature issues on heroku

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -10,14 +10,10 @@ class AdminApplicationsController < ApplicationController
     @pet_application = PetApplication.find(params[:id])
 
     if params[:status] == "rejected"
-      @pet_application.update_attribute(:status, "Rejected") &&
       @application.update_attribute(:status, "Rejected")
     elsif params[:status] == "approved"
-      @pet_application.update_attribute(:status, "Approved") &&
-      @application.update_attribute(:status, "Approved") &&
-      @application.pets.update_all(adoptable: false)
+      @application.update_attribute(:status, "Approved") && @application.pets.update_all(adoptable: false)
     elsif @pets.adoptable == false
-      @pet_application.update_attribute(:status, "Rejected") &&
       @application.update_attribute(:status, "Rejected")
     end
   end
