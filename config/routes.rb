582b9c230fc4d607dde800f8451aa1536831fa69
iPowers <isika.powers@gmail.com>
2021-09-09 09:39:18 -0600

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,10 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
-  get '/pets/:id', to: 'pets#show'
-  get '/pets/:id/edit', to: 'pets#edit'
-  patch '/pets/:id', to: 'pets#update'
-  delete '/pets/:id', to: 'pets#destroy'
+  get '/pets/:pet_id', to: 'pets#show'
+  get '/pets/:pet_id/edit', to: 'pets#edit'
+  patch '/pets/:pet_id', to: 'pets#update'
+  delete '/pets/:pet_id', to: 'pets#destroy'
 
   get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
   get '/shelters/:shelter_id/pets/new', to: 'pets#new'
